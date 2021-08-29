22e536141c17cef54cc477d718c95782b3ed5ab2
ahr1384 <amirhoseinrahimi.work@gmail.com>
2021-08-29 12:44:27 +0430

    fix search page style

--- search/index.php
+++ search/index.php
@@ -7,15 +7,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> search about "<?php echo $_GET["search"]; ?>"</title>
     <link rel="stylesheet" href="./../css/style.css">
+    <link rel="stylesheet" href="./style.css">
 </head>
 <br>
 
-<body style="text-align: center; overflow:hidden;">
-    Your search is "<B style="color: red; font-size: 20px; font-weight:800;"><?php echo $_GET["search"]; ?></B>"<br>
+<body>
+    Your search is "<b><?php echo $_GET["search"]; ?></b>"<br>
     -----------------<br>
     <br>
     <form action="" method="GET">
-        <input type="search" name="search" value="<?php echo $_GET["search"]; ?>" style="padding: 10px 15px; width:40%; outline: none; font-size:16px;">
+        <input type="search" name="search" value="<?php echo $_GET["search"]; ?>">
     </form>
 </body>
 
